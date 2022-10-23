ca1c7e412ee403fd0543fd97f11f82313f484e36
leoframba <89568350+leoframba@users.noreply.github.com>
2022-10-22 19:21:23 -0700

    Config_Parser.c Parses a text file in JSON format into a config settings struct

---
+++ Part1/Config_Parser.c
@@ -0,0 +1,120 @@
+//
+// Created by Leonardo Framba on 10/22/22.
+//
+# include <stdio.h>
+# include "/Users/lframba/CLionProjects/CS336/Final-Project/JSON/cJSON.c"
+
+struct config_settings{
+    char *serverIp;
+    int sourcePort;
+    int destPortUdp;
+    char destPortTcpHead;
+    char destPortTcpTail;
+    int TcpPortNum;
+    int UdpPayLoadSize;
+    int InterTime;
+    long UdpPackCount;
+    int Ttl;
+};
+
+char *file_to_string(FILE *file) {
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    //Allocate buffer
+    char *buffer = malloc(file_size);
+    if (!buffer) {
+        puts("Error: Unable to allocate memory for buffer");
+    }
+
+    //Allocate string
+    char *file_string = malloc(file_size * sizeof(char));
+    if (!file_string) {
+        puts("Error: Unable to allocate memory for file_string");
+    }
+
+    //Write contents of file to string
+    fread(file_string, sizeof(char), file_size, file);
+    fclose(file);
+
+    return file_string;
+}
+
+struct config_settings* json_to_struct(cJSON *json){
+    struct config_settings *settings = malloc(sizeof (struct config_settings));
+    if(!settings){
+        puts("Error: Failed to allocate memory for config settings");
+        exit(0);
+    }
+
+    //TODO there has to be a better way to do this
+    //TODO Check for invalid config vals
+    settings->serverIp = cJSON_GetObjectItemCaseSensitive(json, "serverIp")->valuestring;
+    settings->sourcePort = cJSON_GetObjectItemCaseSensitive(json, "sourcePort")->valueint;
+    settings->destPortUdp = cJSON_GetObjectItemCaseSensitive(json, "destPortUdp")->valueint;
+    settings->destPortTcpHead = (char) cJSON_GetObjectItemCaseSensitive(json, "destPortTcpHead")->valueint;
+    settings->destPortTcpTail = (char) cJSON_GetObjectItemCaseSensitive(json, "destPortTcpTail")->valueint;
+    settings->TcpPortNum = cJSON_GetObjectItemCaseSensitive(json, "TcpPortNum")->valueint;
+
+    //Default val 100
+    settings->UdpPayLoadSize = cJSON_GetObjectItemCaseSensitive(json, "UdpPayloadSize")->valueint ?
+                               cJSON_GetObjectItemCaseSensitive(json, "UdpPayloadSize")->valueint :
+                               100;
+
+    //Default val 15
+    settings->InterTime = cJSON_GetObjectItemCaseSensitive(json, "InterTime")->valueint ?
+                          cJSON_GetObjectItemCaseSensitive(json, "InterTime")->valueint :
+                          15;
+
+    //Default 6000
+    settings->UdpPackCount = cJSON_GetObjectItemCaseSensitive(json, "UdpPackCount")->valueint ?
+                             cJSON_GetObjectItemCaseSensitive(json, "UdpPackCount")->valueint :
+                             6000;
+
+    settings->Ttl = cJSON_GetObjectItemCaseSensitive(json, "Ttl")->valueint ?
+                    cJSON_GetObjectItemCaseSensitive(json, "Ttl")->valueint :
+                    255;
+
+    return settings;
+}
+
+
+
+struct config_settings* parse_config(char *path) {
+    //Invalid path TODO: check vs a defined path name?
+    if (!path) {
+        puts("Error: Invalid config file path");
+        exit(0);
+    }
+
+    //Open config file to read
+    FILE *config = fopen(path, "rb");
+    if(!config){
+        puts("Error: Unable to open config file");
+        exit(0);
+    }
+
+    //Convert file to string
+    char *file_string = file_to_string(config);
+    printf("Successfully loaded config file at: %s\n", path);
+
+    //Test print
+    printf("%s\n", file_string);
+
+    cJSON *json = cJSON_Parse(file_string);
+    if(!json){
+        puts("Error: Failed to parse string to json");
+        exit(0);
+    }
+
+    printf("%s\n", cJSON_Print(json));
+    cJSON *sourcePort = cJSON_GetObjectItemCaseSensitive(json, "serverIp");
+    printf("%s\n", cJSON_GetObjectItemCaseSensitive(json, "serverIp")->valuestring);
+
+    printf("Array Size = %i\n", cJSON_GetArraySize(json));
+
+    return json_to_struct(json);
+}
+
+
--- Part1/Main.c
+++ Part1/Main.c
@@ -1,5 +1,45 @@
 # include <stdio.h>
+# include <stdlib.h>
+# include <strings.h>
+#include <netinet/in.h>
+#include "Config_Parser.c"
+# include "/Users/lframba/CLionProjects/CS336/Final-Project/JSON/cJSON.h"
 
-int main(){
-    printf("Test");
+
+
+int main(int argc, char **argv){
+
+    //Check arg count for 2. 1) program name 2) config file path
+    if(argc < 2){
+        puts("Error: Too Few Args");
+        exit(0);
+    }else if(argc > 2) {
+        puts("Error: Too many Args");
+        exit(0);
+    }
+
+    printf("Arg Count: %i\nConfig file: %s\n", argc, argv[1]);
+    //IF we have correct args. Parse config file which should be arg 2
+
+    //Parse config setting file into a settings struct
+    struct config_settings *settings = parse_config(argv[1]);
+
+
+
+
+    int sock, connId;
+    struct sockaddr_in sAddr, cli;
+
+    //Create Socket -> socket(int domain, int type, int protocol)
+    sock = socket(PF_INET, SOCK_STREAM, PF_UNSPEC);
+
+    if(sock == -1){
+        puts("Failed to create socket!");
+        exit(0);
+    }else{
+        puts("Successfully created socket!");
+    }
+    //memset but assumes starting val is 0
+    //Set all values in mem to null
+    bzero(&sAddr, sizeof (sAddr));
 }
